33153a4158e5897c3fa8ca5b2b17c0ca64e4f71d
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-24 00:25:23 +0330

    Add Student Model with migration

--- src/database/migrations/2023_04_23_204709_create_students_table.php
+++ src/database/migrations/2023_04_23_204709_create_students_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('stn')->unique();
+            $table->string('phone')->unique();
+            $table->string('gender');
             $table->timestamps();
         });
     }
