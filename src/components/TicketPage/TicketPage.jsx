6be5ad0f5e2ef0c9e2666cf7e7e2900ad510dbee
asma <as.ch.2280@gmail.com>
2023-04-25 01:07:29 +0430

    create components for ticket section

---
+++ src/components/TicketPage/TicketPage.jsx
@@ -0,0 +1,5 @@
+const TicketPage = () => {
+  return <div></div>;
+};
+
+export default TicketPage;
---
+++ src/components/TicketPage/TicketPdf/TicketPdf.jsx
@@ -0,0 +1,5 @@
+const TicketPdf = () => {
+  return <div></div>;
+};
+
+export default TicketPdf;
