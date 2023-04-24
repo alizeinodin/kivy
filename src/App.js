b195d88839882e1f862aaae9210373d2faf940ae
asma <as.ch.2280@gmail.com>
2023-04-25 01:21:13 +0430

    add jsx for ticketpage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Header/Nav/Nav";
 import Slider from "./components/IntroducingInstructor/Slider/Slider";
+import TicketPage from "./components/TicketPage/TicketPage";
 function App() {
   return (
-    <Slider />
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<RegisterForm />} />
-    //     <Route path="/Register" element={<RegisterForm />} />
-    //   </Routes>
-    // </Router>
+   
+    <Router>
+      <Routes>
+        <Route path="/" element={<RegisterForm />} />
+        <Route path="/Register" element={<RegisterForm />} />
+        <Route path="/Ticket" element={<TicketPage/>}/>
+      </Routes>
+    </Router>
     // <div className='w-full h-screen bg-[#616473] flex items-center'>
     //   <RegisterForm />
     // </div>
--- src/components/TicketPage/TicketPage.jsx
+++ src/components/TicketPage/TicketPage.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import Ticket from "./Ticket/Ticket";
+import TicketPdf from "./TicketPdf/TicketPdf";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { Link } from "react-router-dom";
 const TicketPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>:) ثبت نام شما با موفقیت انجام شد</h2>
+      <Ticket/>
+      <PDFDownloadLink document={<TicketPdf/>} fileName="ticket.pdf">دانلود بلیط</PDFDownloadLink>
+      <Link to={'/'}>رفتن به صفحه دوره</Link>
+    </div>
+  );
 };
 
 export default TicketPage;
