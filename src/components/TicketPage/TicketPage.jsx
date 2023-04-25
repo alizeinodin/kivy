30b9dcdd0f12053d385ed4ac0a5f1a3585e95db9
asma <as.ch.2280@gmail.com>
2023-04-25 16:51:35 +0430

    add style for ticketPage

--- src/components/TicketPage/TicketPage.jsx
+++ src/components/TicketPage/TicketPage.jsx
@@ -3,13 +3,22 @@ import Ticket from "./Ticket/Ticket";
 import TicketPdf from "./TicketPdf/TicketPdf";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import { Link } from "react-router-dom";
+import './TicketPage.css'
 const TicketPage = () => {
   return (
-    <div>
-      <h2>:) ثبت نام شما با موفقیت انجام شد</h2>
-      <Ticket/>
-      <PDFDownloadLink document={<TicketPdf/>} fileName="ticket.pdf">دانلود بلیط</PDFDownloadLink>
-      <Link to={'/'}>رفتن به صفحه دوره</Link>
+    <div className="TicketPage">
+      <h2 className="TicketPage-title"> ثبت نام شما با موفقیت انجام شد</h2>
+      <Ticket />
+      <PDFDownloadLink
+        document={<TicketPdf />}
+        fileName="ticket.pdf"
+        className="TicketPage-downloadLink"
+      >
+        دانلود بلیط
+      </PDFDownloadLink>
+      <Link to={"/"} className="TicketPage-link">
+        رفتن به صفحه دوره
+      </Link>
     </div>
   );
 };
