5488a26c3a397e600bae95c382de9e6dc41fdfa0
asma <as.ch.2280@gmail.com>
2023-04-26 00:18:45 +0430

    add jsx for IntroducingInstructor

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Header/Nav/Nav";
 import Slider from "./components/IntroducingInstructor/Slider/Slider";
 import TicketPage from "./components/TicketPage/TicketPage";
+import IntroducingInstructor from './components/IntroducingInstructor/IntroducingInstructor'
 function App() {
   return (
-   
-    <Router>
-      <Routes>
-        <Route path="/" element={<RegisterForm />} />
-        <Route path="/Register" element={<RegisterForm />} />
-        <Route path="/Ticket" element={<TicketPage/>}/>
-      </Routes>
-    </Router>
+   <IntroducingInstructor/>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<RegisterForm />} />
+    //     <Route path="/Register" element={<RegisterForm />} />
+    //     <Route path="/Ticket" element={<TicketPage/>}/>
+    //   </Routes>
+    // </Router>
     // <div className='w-full h-screen bg-[#616473] flex items-center'>
     //   <RegisterForm />
     // </div>
--- src/components/IntroducingInstructor/IntroducingInstructor.jsx
+++ src/components/IntroducingInstructor/IntroducingInstructor.jsx
@@ -1,5 +1,20 @@
+import React, { Fragment, useRef } from "react";
+import Slider from "./Slider/Slider";
+import icon from "./../../imgs/tempimg/abstract.png";
+import teacher from "./../../imgs/tempimg/400x500 (2).png";
 const IntroducingInstructor = () => {
-  return <div></div>;
+  return (
+    <div className="IntroducingInstructor">
+      <div className="IntroducingInstructor-header">
+        <img src={icon} className="IntroducingInstructor-header-img" />
+        <p className="IntroducingInstructor-header-title">معرفی مدرس</p>
+      </div>
+      <div className="IntroducingInstructor-content">
+        <img src={teacher} className="IntroducingInstructor-content-img" />
+        <Slider />
+      </div>
+    </div>
+  );
 };
 
 export default IntroducingInstructor;
