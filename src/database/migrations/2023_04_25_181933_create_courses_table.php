84bda3dd698ccd7018cb789221a8a7d38d2ff76e
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-25 22:05:47 +0330

    Add columns for courses table

--- src/database/migrations/2023_04_25_181933_create_courses_table.php
+++ src/database/migrations/2023_04_25_181933_create_courses_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('logo')->nullable();
+            // seo section
+            $table->string('seo_title');
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->enum('status')->default('DRAFT');
             $table->timestamps();
         });
     }
