12556d233bf89c065e1041e4d5ef48012f21850b
asma <as.ch.2280@gmail.com>
2023-04-25 18:34:04 +0430

    add style for ticket

--- src/components/TicketPage/Ticket/Ticket.jsx
+++ src/components/TicketPage/Ticket/Ticket.jsx
@@ -1,5 +1,9 @@
 import TicketDetail from "./TicketDetail/TicketDetail";
 import React, { useState } from "react";
+import "./Ticket.css";
+import logo from "./../../../imgs/tempimg/back-in-time.png";
+import QR from "./../../../imgs/tempimg/qr-code.png";
+import { RxCopy, RxCheck } from "react-icons/rx";
 const Ticket = () => {
   const [detail, setDetail] = useState([
     {
@@ -19,10 +23,12 @@ const Ticket = () => {
     },
     { id: 4, title: "مدرس دوره", content: "محمد مرادی " },
   ]);
+  const [code, setCode] = useState("123698532585757");
+  const [isCopy, setIscopy] = useState(false);
   return (
     <div className="ticket">
       <div className="ticket-header">
-        <img className="ticket-header-img" />
+        <img className="ticket-header-img" src={logo} />
         <div className="ticket-header-content">
           <span className="ticket-header-title">بلیط حضور در</span>
           <h1 className="ticket-header-heading">دوره آموزش فتوشاپ</h1>
@@ -31,20 +37,37 @@ const Ticket = () => {
       {/* --------------------------------------------------- */}
       <div className="ticket-detail">
         {detail.map((item) => (
-          <TicketDetail key={item.id} {...item} />
+          <TicketDetail
+            key={item.id}
+            {...item}
+            className="ticket-detail-item"
+          />
         ))}
       </div>
       {/* --------------------------------------------- */}
-      <div className="ticket-line"></div>
+      <div className="ticket-line">
+        <span className="circle-right"></span>
+        <span className="line"></span>
+        <span className="circle-left"></span>
+      </div>
       {/* --------------------------------------------------- */}
 
       <div className="ticket-barcode">
-        <img src="" alt="" className="ticket-barcode-img" />
+        <img src={QR} alt="" className="ticket-barcode-img" />
         <div className="ticket-barcode-code">
-          <span>کد رهگیری ثبت نام</span>
-          <div className="barcode">
-            <span> 1236985325 </span>
-            {/* icon for copy */}
+          <span className="ticket-barcode-code-title">کد رهگیری ثبت نام</span>
+          <div
+            className="barcode"
+            onClick={() => {
+              navigator.clipboard.writeText(code).then(() => setIscopy(true));
+            }}
+          >
+            <span className="barcode-code"> {code} </span>
+            {isCopy ? (
+              <RxCheck className="copy-icon" />
+            ) : (
+              <RxCopy className="copy-icon" />
+            )}
           </div>
         </div>
       </div>
