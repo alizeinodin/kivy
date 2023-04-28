4804fdb33829cd2f54af7c9d635657c61b01c011
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-28 17:42:55 +0330

    Add pay route

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PayController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(PayController::class)->group(function () {
+    Route::prefix('/payment')->group(function () {
+        Route::name('payment')->group(function () {
+            Route::get('/{student}/{price}/{detail}', 'pay')
+                ->name('pay');
+        });
+    });
 });
