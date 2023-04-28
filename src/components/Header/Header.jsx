b4ba5820b361939549986ef86f5c2fcb3db873a5
asma <as.ch.2280@gmail.com>
2023-04-28 23:10:21 +0430

    add scroll functions in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import Nav from "./Nav/Nav";
 // import { Link } from "react-router-dom";
 import React from "react";
-import './Header.css'
-const Header = () => {
+import "./Header.css";
+const Header = ({
+  ScrollCourseIntroductionRef,
+  ScrollCourseTopicsRef,
+  ScrollIntroducingInstructorRef,
+  ScrollFooterRef,
+}) => {
   return (
     <div className="Header">
-      <Nav />
+      <Nav
+        ScrollCourseIntroductionRef={ScrollCourseIntroductionRef}
+        ScrollCourseTopicsRef={ScrollCourseTopicsRef}
+        ScrollIntroducingInstructorRef={ScrollIntroducingInstructorRef}
+        ScrollFooterRef={ScrollFooterRef}
+      />
       <div className="Header-content">
         <h1 className="Header-title">دوره آموزش جامع فتوشاپ از صفر تا صد</h1>
         <p className="Header-description">
--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,24 +1,12 @@
 import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
 import "./Nav.css";
-const Nav = () => {
+const Nav = ({
+  ScrollCourseIntroductionRef,
+  ScrollCourseTopicsRef,
+  ScrollIntroducingInstructorRef,
+  ScrollFooterRef,
+}) => {
   const navContainer = useRef(null);
-  // useLayoutEffect(() => {
-  //   const navTop = navContainer.current.getBoundingClientRect().top;
-  //   const onScroll = () => {
-  //     if (navTop < window.scrollY && window.innerWidth > 768) {
-  //       console.log("ok");
-  //       navContainer.current.style.position = "fixed";
-  //       navContainer.current.style.zIndex = 1000;
-  //       navContainer.current.style.width = "100vw";
-  //       navContainer.current.style.marginTop = "0";
-  //     } else {
-  //       navContainer.current.style.position = "relative";
-  //     }
-  //   };
-  //   window.addEventListener("scroll", onScroll);
-  //   return () => window.removeEventListener("scroll", onScroll);
-  // }, []);
-
   const linkRef = useRef(null);
 
   const [showMenu, setShowMenu] = useState(false);
@@ -30,7 +18,7 @@ const Nav = () => {
       <div
         className="NavBtn"
         onClick={() => {
-          linkRef.current.style.display = showMenu ? "none": "flex" ;
+          linkRef.current.style.display = showMenu ? "none" : "flex";
           setShowMenu(!showMenu);
         }}
       >
@@ -41,16 +29,44 @@ const Nav = () => {
       <ul className="NavLinks" ref={linkRef}>
         <li
           className="NavLink"
-          onClick={() => {
-            console.log("here");
-          }}
+          // onClick={() => {
+          //   console.log("here");
+          // }}
         >
           کیوی
         </li>
-        <li className="NavLink">معرفی دوره</li>
-        <li className="NavLink">سرفصل‌های دوره</li>
-        <li className="NavLink">مدرس دوره</li>
-        <li className="NavLink">راه‌های ارتباطی</li>
+        <li
+          className="NavLink"
+          onClick={() => {
+            ScrollCourseIntroductionRef();
+          }}
+        >
+          معرفی دوره
+        </li>
+        <li
+          className="NavLink"
+          onClick={() => {
+            ScrollCourseTopicsRef();
+          }}
+        >
+          سرفصل‌های دوره
+        </li>
+        <li
+          className="NavLink"
+          onClick={() => {
+            ScrollIntroducingInstructorRef();
+          }}
+        >
+          مدرس دوره
+        </li>
+        <li
+          className="NavLink"
+          onClick={() => {
+            ScrollFooterRef();
+          }}
+        >
+          راه‌های ارتباطی
+        </li>
         <li className="NavLink">ثبت نام</li>
       </ul>
     </nav>
