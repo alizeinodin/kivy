6005b570eaa60b4059280fd4ab8eb6001a586cfa
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-28 21:07:46 +0330

    Add PayController and pay method

--- src/app/Http/Controllers/PayController.php
+++ src/app/Http/Controllers/PayController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Pay;
+use App\Models\Student;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Payment\Facade\Payment;
 
 class PayController extends Controller
 {
-    //
+    protected string $callbackRoute = '';
+
+    public function pay(Student $student, int $price, string $detail = null)
+    {
+        // invoice
+        $invoice = new Invoice();
+        $invoice->amount($price);
+        $invoice->detail([
+            'description' => $detail,
+        ]);
+
+        return Payment::callbackUrl($this->callbackRoute)->purchase(
+            $invoice,
+            function ($driver, $transactionId) use ($student, $price) {
+                // add pay to db
+                $pay = new Pay();
+                $pay->amount = $price;
+                $pay->trans_id = $transactionId;
+
+                $student->pays()->save($pay);
+            }
+        )->pay()->toJson;
+
+    }
 }
