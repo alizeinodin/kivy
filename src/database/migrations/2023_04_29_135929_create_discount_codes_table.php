9097927b15b93ecc3f4173b597b28eba7204e29f
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-29 17:38:22 +0330

    Add columns for discount_codes table

--- src/database/migrations/2023_04_29_135929_create_discount_codes_table.php
+++ src/database/migrations/2023_04_29_135929_create_discount_codes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('discount_codes', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
+            $table->decimal('amount', 8, 2);
+            $table->integer('uses')->nullable();
+            $table->dateTime('expires_at')->nullable(); 
             $table->timestamps();
         });
     }
