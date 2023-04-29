a4f110dffca8c04e5d29022190f02d43f629f1c4
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-29 09:50:17 +0330

    Add StoreRequest for Student

---
+++ src/app/Http/Requests/Student/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'stn' => 'integer',
+            'phone' => 'required|max:20|regex:^(\+98|0)?9\d{9}$|',
+            'gender' => 'required|in:male,female',
+        ];
+    }
+}
