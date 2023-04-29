37406002a01b64e6818833657eb83b092e00d871
Ali Zeinodin <alizeinodin79@gmail.com>
2023-04-29 10:09:13 +0330

    Add store method in StudnetController

--- src/app/Http/Controllers/StudentController.php
+++ src/app/Http/Controllers/StudentController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Student\StoreRequest;
+use App\Models\Student;
+use Illuminate\Http\JsonResponse;
 
 class StudentController extends Controller
 {
-    // TODO add store method
+    public function store(StoreRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        // get student
+        $student = Student::where(['phone' => $validatedData['phone']])
+            ->latest()
+            ->first();
+
+
+        if (empty($student)) {
+            $student = Student::create([
+                'name' => $validatedData['name'],
+                'stn' => $validatedData['stn'] ?? null,
+                'phone' => $validatedData['phone'],
+                'gender' => $validatedData['gender']
+            ]);
+        }
+
+        return response()->json($student);
+    }
 }
