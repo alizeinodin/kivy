a94729a9b900ccbde6caa71bf14c298950203411
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-02 21:16:05 +0330

    Develop rules for CallbackRequest

--- src/app/Http/Requests/Pay/CallbackRequest.php
+++ src/app/Http/Requests/Pay/CallbackRequest.php
@@ -13,7 +13,7 @@ class CallbackRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class CallbackRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'id' => 'required|integer',
+            'amount' => 'required|integer',
+            'student_id' => 'required|integer',
         ];
     }
 }
