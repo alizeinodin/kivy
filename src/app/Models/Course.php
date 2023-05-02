512d74d7e44184b27b2fc8f5ba30f38b27b05b3b
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-02 23:32:12 +0330

    Add pays method and students method

--- src/app/Models/Course.php
+++ src/app/Models/Course.php
@@ -10,6 +10,18 @@ class Course extends Model
 {
     use HasFactory;
 
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'pays')
+            ->withTimestamps()
+            ->withPivot(['amount', 'discount_code_id']);
+    }
+
+    public function pays(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Pay::class);
+    }
+
     public function discountCodes(): BelongsToMany
     {
         return $this->belongsToMany(DiscountCode::class);
