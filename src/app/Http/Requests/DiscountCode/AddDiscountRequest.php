c1d153fa5ec5986571dc53506c23ad10a71759ea
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-02 23:57:31 +0330

    Rename AddDiscount Request

--- src/app/Http/Requests/DiscountCode/AddDiscountRequest.php
+++ src/app/Http/Requests/DiscountCode/AddDiscountRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\DiscountCode;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AddDiscount extends FormRequest
+class AddDiscountRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
