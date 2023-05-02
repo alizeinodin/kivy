c5c20bae01b424d1032c06489656681a15f34108
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-02 13:13:09 +0330

    Add get method in TicketController

--- src/app/Http/Controllers/TicketController.php
+++ src/app/Http/Controllers/TicketController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Course;
+use App\Models\Student;
 
 class TicketController extends Controller
 {
-    //
+    // TODO implement api for get name of student and course and qrcode
+    public function get(Student $student, Course $course)
+    {
+
+    }
 }
