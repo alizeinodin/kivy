7466daf4c7a70ca16b84064c444623d248dfb8d0
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-02 23:30:28 +0330

    Add courses method in Student model

--- src/app/Models/Student.php
+++ src/app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
@@ -20,5 +21,12 @@ class Student extends Model
     public function pays(): HasMany
     {
         return $this->hasMany(Pay::class);
+    }
+
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'pays')
+            ->withTimestamps()
+            ->withPivot(['amount', 'discount_code_id']);
     }
 }
