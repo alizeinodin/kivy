0611b58007e62b66f855923d2143347dc32891a9
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-10 19:24:29 +0330

    Add get method in TicketController

--- src/app/Http/Controllers/TicketController.php
+++ src/app/Http/Controllers/TicketController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Student;
+use Illuminate\Http\JsonResponse;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Symfony\Component\HttpFoundation\Response;
 
 class TicketController extends Controller
 {
-    // TODO implement api for get name of student and course and qrcode
-    public function get(Student $student, Course $course)
+    private string $verifyRoute = 'api.ticket.verify';
+
+    public function get(Student $student, Course $course): JsonResponse
     {
 
+        $path = public_path('images/qrcode/' . mt_rand() . '.png');
+
+        $qrcode = Qrcode::format('png')
+            ->generate('test', $path);
+
+        $response = [
+            'student_name' => $student->name,
+            'course_name' => $course->title,
+            'qrcode' => $path,
+        ];
+
+        return response()
+            ->json($response, Response::HTTP_OK);
     }
 }
