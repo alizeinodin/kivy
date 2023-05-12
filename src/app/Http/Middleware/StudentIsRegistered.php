49ebd3256f61427ebbfa5ac9929609a96a4967df
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-12 10:14:13 +0330

    Fix StudentIsRegistered middleware

--- src/app/Http/Middleware/StudentIsRegistered.php
+++ src/app/Http/Middleware/StudentIsRegistered.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Student;
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class StudentIsRegistered
 {
@@ -13,14 +15,29 @@ class StudentIsRegistered
      * @param \Illuminate\Http\Request $request
      * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      *
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\JsonResponse
      */
     public function handle(Request $request, Closure $next)
     {
         $student = $request->route()->parameter('student');
         $course = $request->route()->parameter('course');
 
-        dd($student, $course);
+        $accepted = Student::whereHas('courses', function ($query) use ($course) {
+            $query->where([
+                'courses.id' => $course,
+                'pays.status' => 'accepted'
+            ]);
+        })
+            ->where(['id' => $student->id])
+            ->first();
+
+        if (!$accepted) {
+            $response = [
+                'message' => 'The student not registered!',
+            ];
+
+            return response()->json($response, Response::HTTP_UNAUTHORIZED);
+        }
         return $next($request);
     }
 }
