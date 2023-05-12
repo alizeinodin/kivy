1014f8a416147b64feb77244a4c5c0334e62e814
Ali Zeinodin <alizeinodin79@gmail.com>
2023-05-12 10:44:24 +0330

    Fix verify method in TicketController

--- src/app/Http/Controllers/TicketController.php
+++ src/app/Http/Controllers/TicketController.php
@@ -38,7 +38,10 @@ class TicketController extends Controller
         $courseId = $course->id;
 
         $student = Student::whereHas('courses', function ($query) use ($courseId) {
-            $query->where('courses.id', $courseId);
+            $query->where([
+                'courses.id' => $courseId,
+                'pays.status' => 'accepted'
+            ]);
         })
             ->where(['id' => $student->id])
             ->first();
