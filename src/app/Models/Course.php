21d8eef7eced7dc71982a7ebb7b653e4cd10e35a
mkoushan <mkoushan@proton.me>
2023-07-27 23:58:35 +0330

    Add course_date column to Course table

--- src/app/Models/Course.php
+++ src/app/Models/Course.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Morilog\Jalali\Jalalian;
 
 class Course extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['course_date'];
 
     public function students(): BelongsToMany
     {
@@ -25,5 +28,15 @@ class Course extends Model
     public function discountCodes(): BelongsToMany
     {
         return $this->belongsToMany(DiscountCode::class);
+    }
+
+    public function getCourseDateAttribute($value)
+    {
+        return Jalalian::fromCarbon($value)->format('Y-m-d');
+    }
+
+    public function setCourseDateAttribute($value)
+    {
+        $this->attributes['course_date'] = Jalalian::fromFormat('Y-m-d', $value)->toCarbon();
     }
 }
